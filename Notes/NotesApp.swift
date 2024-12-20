ea4d44106977b60e6c3fb44a419075f29a7b22a5
Gonzalo Lozg <gonzalo.lozg@gmail.com>
2024-12-20 13:21:52 -0500

    init

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Gonzalo on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
