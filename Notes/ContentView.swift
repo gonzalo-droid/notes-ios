60d7c1c7f3dd2621b561d4f0c8e30c72636d18c7
Gonzalo Lozg <gonzalo.lozg@gmail.com>
2024-12-20 13:48:04 -0500

    feat: image shape

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -10,10 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .background(Color.blue)
+                .padding()
+                .background(Color.red)
+                .frame(width: 100, height: 100)
+            
+            Rectangle()
+                .frame(width: 50, height: 50)
+                .foregroundStyle(Color.yellow)
+                .cornerRadius(100)
+                .opacity(0.5)
+            
+            Ellipse()
+                .stroke(lineWidth: 5)
+                .frame(width: 50, height: 100)
+                .foregroundStyle(Color.red)
+                .rotationEffect(.degrees(50))
+                
+            
+            Text("Hello, Platzi! buenas buenas")
+                .font(.system(size: 30))
+                .lineLimit(2)
+                .multilineTextAlignment(.trailing)
+                .bold()
+                .underline()
+            
+            
         }
         .padding()
     }
