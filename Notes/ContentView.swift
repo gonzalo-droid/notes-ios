291de71a0f64ccd36d5d39de8407e0e6f79ad96b
Gonzalo Lozg <gonzalo.lozg@gmail.com>
2024-12-20 14:10:37 -0500

    feat: types Stack

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -9,32 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Circle()
-                .background(Color.blue)
-                .padding()
-                .background(Color.red)
+        VStack(alignment: .center, spacing: 20) {
+            
+            ZStack() {
+                Circle()
+                    .background(Color.blue)
+                    .padding()
+                    .background(Color.red)
+                    .frame(width: 100, height: 100)
+                
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                    .foregroundStyle(Color.red)
+            }
+            
+            
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
                 .frame(width: 100, height: 100)
-            
-            Rectangle()
-                .frame(width: 50, height: 50)
-                .foregroundStyle(Color.yellow)
-                .cornerRadius(100)
-                .opacity(0.5)
-            
-            Ellipse()
-                .stroke(lineWidth: 5)
-                .frame(width: 50, height: 100)
-                .foregroundStyle(Color.red)
-                .rotationEffect(.degrees(50))
                 
             
             Text("Hello, Platzi! buenas buenas")
                 .font(.system(size: 30))
                 .lineLimit(2)
-                .multilineTextAlignment(.trailing)
+                .multilineTextAlignment(.center)
                 .bold()
                 .underline()
+            
+            Button(action: {
+                    print("Button with icon tapped!")
+                }) {
+                    HStack(
+                        alignment: .center, spacing: 10
+                    ) {
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(.yellow)
+                        Text("Favorite")
+                            .fontWeight(.bold)
+                    }
+                    .padding()
+                    .background(Color.purple)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
             
             
         }
